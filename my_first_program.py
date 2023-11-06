546cac82f11a94f6edd463b5df9a5f5fa6a6c6a0
Haspp <107359050+Haspp@users.noreply.github.com>
2023-11-06 19:09:40 +0300

    first commit

---
+++ my_first_program.py
@@ -0,0 +1,5 @@
+# Начало и объявление переменных
+first_name, second_name, surname = "Евгений", "Солоницын", "Андреевич"
+
+# Вывод с помощью F-cтроки
+print(f"Здравствуйте, Меня зовут {second_name} {first_name} {surname}.")
